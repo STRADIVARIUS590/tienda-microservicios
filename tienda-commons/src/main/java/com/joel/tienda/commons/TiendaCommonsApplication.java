2090d894b9bfab4f31e0fc5232f99aa630710c95
Jvel <joel.uss1728@gmail.com>
2025-04-21 09:07:26 -0600

    common

---
+++ tienda-commons/src/main/java/com/joel/tienda/commons/TiendaCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.joel.tienda.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCommonsApplication.class, args);
+	}
+
+}
---
+++ tienda-commons/src/test/java/com/joel/tienda/commons/TiendaCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.tienda.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
