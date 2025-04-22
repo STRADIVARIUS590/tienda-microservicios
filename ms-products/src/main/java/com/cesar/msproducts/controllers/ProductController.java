17b2f9c89aa0ca71805d5c876dcce6784872db1a
Cesar Ojeda Rios <95256915+CesarArmando95@users.noreply.github.com>
2025-04-21 23:54:45 -0600

    Corrigiendo nombre del controlado de productosr

--- ms-products/src/main/java/com/cesar/msproducts/controllers/ProductController.java
+++ ms-products/src/main/java/com/cesar/msproducts/controllers/ProductController.java
@@ -3,14 +3,13 @@ package com.cesar.msproducts.controllers;
 import com.cesar.msproducts.dto.ProductDTO;
 import com.cesar.msproducts.services.ProductService;
 import com.joel.tienda.commons.controllers.CommonController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/api/productos")
-public class ProdcuctController extends CommonController<ProductDTO, ProductService> {
+public class ProductController extends CommonController<ProductDTO, ProductService> {
 
-    public ProdcuctController(ProductService service) {
+    public ProductController(ProductService service) {
         super(service);
     }
 }
